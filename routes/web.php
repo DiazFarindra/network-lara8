9dbb6218761c8ad34e052ad68c02e5060b1747b7
diaz farindra <farindrad@gmail.com>
2021-08-24 15:41:10 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\TimelineController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/timeline', TimelineController::class)->name('timeline');
+});
+
+require __DIR__.'/auth.php';
